a567951f3f42d3fe1b50e9fcfe2aee5f4214f9f2
Carlos <arlo252@gmail.com>
2023-10-03 23:48:14 -0400

    playing with ngFor & ng If in Angular

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -10,5 +10,17 @@ export class FirstComponent {
   name: string = "";
   email: string = "";
   message: string = "";
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
 
+  onSubmit() : void {
+    this.isSubmitted = true;
+    this.messages.push({
+      "name": this.name,
+      "email": this.email,
+      "message": this.message
+    });
+    console.log(this.messages);
+    
+  };
 }
