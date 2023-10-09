fed4113ed21c30dca6e74a02945ff6821c51a273
Carlos <arlo252@gmail.com>
2023-10-09 12:15:56 -0400

    add menu and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { FormsModule } from '@angular/forms';
 import { MessageDetailsComponent } from './components/message-details/message-details.component';
+import { SsuperService } from './services/ssuper.service';
+import { MenuComponent } from './components/menu/menu.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    MessageDetailsComponent
+    MessageDetailsComponent,
+    MenuComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [SsuperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
